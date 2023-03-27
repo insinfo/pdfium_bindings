8d6a19801a70ac7fb929c2c9ae12432dbff365da
Julio Rios <jrp8900@gmail.com>
2023-03-26 20:23:31 -0500

    Fix lint issues for examples

--- example/example3.dart
+++ example/example3.dart
@@ -1,12 +1,15 @@
-import 'package:pdfium_bindings/pdfium_bindings.dart';
 import 'package:http/http.dart' as http;
+import 'package:pdfium_bindings/pdfium_bindings.dart';
 
 void main() async {
-  var pdfium = PdfiumWrap();
+  final pdfium = PdfiumWrap();
 
-  var resp = await http.get(Uri.parse(
-      'https://www.riodasostras.rj.gov.br/wp-content/uploads/2022/03/1426.pdf'));
-  var bytes = resp.bodyBytes;
+  final resp = await http.get(
+    Uri.parse(
+      'https://www.riodasostras.rj.gov.br/wp-content/uploads/2022/03/1426.pdf',
+    ),
+  );
+  final bytes = resp.bodyBytes;
 
   pdfium
       .loadDocumentFromBytes(bytes)
