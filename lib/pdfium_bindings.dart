1e0b3f55627014897ac8b97166a0a16d1eb1bbd8
Isaque Neves <isaque.neves@riodasostras.rj.gov.br>
2025-11-11 19:47:31 -0300

    fix exports

--- lib/pdfium_bindings.dart
+++ lib/pdfium_bindings.dart
@@ -3,3 +3,5 @@ export 'src/extensions.dart';
 export 'src/pdfium_bindings.dart';
 export 'src/pdfium_wrap.dart';
 export 'src/utils.dart';
+export 'src/utils/pdfium_service.dart';
+export 'src/utils/pdfium_service_mutex.dart';
